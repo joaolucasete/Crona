1b9ab1350302b15c41b171384113794ec301299c
chiyoku <chiyokuoni@gmail.com>
2020-06-18 18:12:21 -0300

    Fixing factor function

--- gorgon/src/parser/expr.rs
+++ gorgon/src/parser/expr.rs
@@ -1,10 +1,10 @@
+use super::NodeKind;
 use crate::BinKind;
 use crate::CompilerError;
 use crate::Node;
 use crate::Parser;
-use crate::TokenKind;
 use crate::Token;
-use super::NodeKind;
+use crate::TokenKind;
 use std::boxed::Box;
 
 fn get_priority(token: BinKind) -> u8 {
@@ -38,18 +38,12 @@ impl<'a> Parser<'a> {
             Some(Token { kind, .. }) => match kind {
                 TokenKind::Number => {
                     self.advance()?;
-                    return Ok(
-                        Node::new(
-                            NodeKind::Number,
-                            self.actual_span()
-                        )
-                    )
-                },
-                _ => {}
+                    return Ok(Node::new(NodeKind::Number, self.actual_span()));
+                }
+                _ => Err(self.unexpected()),
             },
-            _ => {}
+            _ => Err(self.unexpected()),
         }
-        Err(CompilerError::UnfinishedString)
     }
 
     pub fn expr(&mut self, priority: u8) -> Result<Node, CompilerError> {
@@ -57,19 +51,19 @@ impl<'a> Parser<'a> {
 
         if priority == 6 {
             return self.factor();
-        } 
+        }
 
         let mut left = self.expr(priority + 1)?;
-        while let Some(Token { kind: BinToken( bintkn ), ..}) = self.next {
+        while let Some(Token {kind: BinToken(bintkn),..}) = self.next {
             if priority != get_priority(bintkn) {
                 break;
-            } else{
+            } else {
                 self.advance()?;
                 let right = self.expr(priority + 1)?;
-                let span = Parser::mix_span(left.span,right.span);
+                let span = Parser::mix_span(left.span, right.span);
                 left = Node::new(
-                    NodeKind::Binary(Box::new(left),bintkn,Box::new(right)),
-                    span
+                    NodeKind::Binary(Box::new(left), bintkn, Box::new(right)),
+                    span,
                 )
             }
         }
